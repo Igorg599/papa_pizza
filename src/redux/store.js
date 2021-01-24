1195746fc690d132a82a34ee78d4404ea8684931
Igorg599 <igorg599@gmail.com>
2021-01-24 18:39:46 +0500

    commit-32

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,13 +2,21 @@ import {createStore, compose, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
 
 import rootReducer from './reducers';
+import {loadState, saveState} from './localstorage';
+
+const persistedState = loadState();
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 
 const store = createStore(
     rootReducer,
-    composeEnhancers(applyMiddleware(thunk))    
+    persistedState,
+    composeEnhancers(applyMiddleware(thunk)) , 
 );
+
+store.subscribe(() => {
+    saveState(store.getState());
+})
 
 export default store;
