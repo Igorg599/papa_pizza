870b7f8097f9b9d04a389faec410344c689863bb
Igorg599 <igorg599@gmail.com>
2021-02-11 22:15:13 +0500

    commit-37

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { Link } from 'react-router-dom';
+import Phone from '../assets/img/phone.svg';
 
 
 function Categories({activeCategory, items, onClickCategory}) {
@@ -24,6 +25,8 @@ function Categories({activeCategory, items, onClickCategory}) {
                     <li className={activeCategory === index ? 'active' : ''} onClick={() => onClickCategory(index)}>{item.name}</li>
                   </Link>
                 ))}
+                <a class="categories_phone" href="tel:83422318884"><img src={Phone} alt="#"/>+7 (342) 231-88-84</a>
+                <div class="categories_phone">Адрес: г. Пермь, ул. Куйбышева, 87</div>
                 <Link to='/politics' onClick={() => setHamburgerActive(false)}>
                   <div className="categories_politics">Политика конфиденциальности</div>                
                 </Link>
