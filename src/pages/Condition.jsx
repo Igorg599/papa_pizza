f03b09687db4d0e0da84c7ab0792e754f38e3137
Igorg599 <igorg599@gmail.com>
2021-02-20 21:15:39 +0500

    commit-40

--- src/pages/Condition.jsx
+++ src/pages/Condition.jsx
@@ -6,9 +6,13 @@ function Condition() {
             <div className="container">
                 <h2 className="about__title">Доставка</h2>
                 <div className="about__des">Для того, чтобы узнать примерное время ожидания и условия доставки, кликните по району, в который необходимо привезти Ваш заказ.</div>
+                <div className="about__des">Минимальная сумма заказа 500 - 700 рублей(см. карту доставок).</div>
+                <div className="about__des">В пределах представленной карты доставка бесплатная (в зависимости от удалённости и минимальной суммы заказа.</div>
+                <div className="about__des">Сделать заказ можно в вс-чт с 11-00 до 23-00 и в пт-сб с 11-00 до 01-00 по телефону 231-88-84 и на сайте papapizza59.ru</div>
+                <div className="about__des">Время доставки от 60 до 120 минут (зависит от удалённости и дорожной ситуации).</div>
                 <div className="about__des">Время ожидания заказа может быть увеличено в зависимости от ситуации на дорогах и загруженности кухни. Просим обратить внимание на то, что в минимальную сумму заказа с учётом всех скидок,а также промокодов и сертификатов не входят десерты, напитки и соусы.</div>
                 <div className="about__des">А если Вы находитесь неподалёку и желаете сэкономить 20%, то заберите заказ сами по адресу: ул. Куйбышева, 87</div>
-                <div ><a href="https://yandex.ru/maps/50/perm/?utm_medium=mapframe&utm_source=maps"></a><a href="https://yandex.ru/maps/50/perm/?from=api-maps&l=trf%2Ctrfe&ll=56.266246%2C57.985012&mode=usermaps&origin=jsapi_2_1_78&um=constructor%3A1964e7aad7a04da33338dc58cbc20759a481a92d632d23f2cd0a29fa5ad07baf&utm_medium=mapframe&utm_source=maps&z=12.34" ></a><iframe src="https://yandex.ru/map-widget/v1/-/CCUI7Qep9D" width="90%" height="40%" frameborder="1" title='condition' allowfullscreen="true" ></iframe></div>
+                <div ><a href="https://yandex.ru/maps/50/perm/?utm_medium=mapframe&utm_source=maps"></a><a href="https://yandex.ru/maps/50/perm/?from=api-maps&l=trf%2Ctrfe&ll=56.266246%2C57.985012&mode=usermaps&origin=jsapi_2_1_78&um=constructor%3A1964e7aad7a04da33338dc58cbc20759a481a92d632d23f2cd0a29fa5ad07baf&utm_medium=mapframe&utm_source=maps&z=12.34" ></a><iframe src="https://yandex.ru/map-widget/v1/-/CCUI7Qep9D" width="90%" height="300vw" frameborder="1" title='condition' allowfullscreen="true" ></iframe></div>
             </div>
         </div>
     )
